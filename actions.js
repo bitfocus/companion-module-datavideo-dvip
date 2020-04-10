92a6a801ee67be0bae4c0206fbaa08cecf1b47cb
BB21B <50427842+BB21B@users.noreply.github.com>
2020-04-10 23:40:03 +0100

    Allow user to be loaded/saved with numeric input

--- actions.js
+++ actions.js
@@ -39,122 +39,122 @@ exports.getActions = function () {
 			}
 		]
 	};
-	
+
 	if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
-	actions['switch_key2'] = {
-		label: 'Switch Key 2 Aux',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchkey2',
-				default: '0',
-				choices: this.model.key2
-			}
-		]
-	};
-}
+		actions['switch_key2'] = {
+			label: 'Switch Key 2 Aux',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchkey2',
+					default: '0',
+					choices: this.model.key2
+				}
+			]
+		};
+	}
 	if (this.config.modelID == 'se3200') {
-	actions['switch_key3'] = {
-		label: 'Switch Key 3 Aux',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchkey3',
-				default: '0',
-				choices: this.model.key3
-			}
-		]
-	};
-}
+		actions['switch_key3'] = {
+			label: 'Switch Key 3 Aux',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchkey3',
+					default: '0',
+					choices: this.model.key3
+				}
+			]
+		};
+	}
 
-if (this.config.modelID == 'se3200') {
-	actions['switch_key4'] = {
-		label: 'Switch Key 4 Aux',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchkey4',
-				default: '0',
-				choices: this.model.key4
-			}
-		]
-	};
-}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_key4'] = {
+			label: 'Switch Key 4 Aux',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchkey4',
+					default: '0',
+					choices: this.model.key4
+				}
+			]
+		};
+	}
 
-if (this.config.modelID == 'se3200') {
-	actions['switch_aux1'] = {
-		label: 'Switch Aux 1',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchaux1',
-				default: '0',
-				choices: this.model.aux1
-			}
-		]
-	};
-}
-if (this.config.modelID == 'se3200') {
-	actions['switch_aux2'] = {
-		label: 'Switch Aux 2',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchaux2',
-				default: '0',
-				choices: this.model.aux2
-			}
-		]
-	};
-}
-if (this.config.modelID == 'se3200') {
-	actions['switch_aux3'] = {
-		label: 'Switch Aux 3',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchaux3',
-				default: '0',
-				choices: this.model.aux3
-			}
-		]
-	};
-}
-if (this.config.modelID == 'se3200') {
-	actions['switch_aux4'] = {
-		label: 'Switch Aux 4',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchaux4',
-				default: '0',
-				choices: this.model.aux4
-			}
-		]
-	};
-}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_aux1'] = {
+			label: 'Switch Aux 1',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchaux1',
+					default: '0',
+					choices: this.model.aux1
+				}
+			]
+		};
+	}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_aux2'] = {
+			label: 'Switch Aux 2',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchaux2',
+					default: '0',
+					choices: this.model.aux2
+				}
+			]
+		};
+	}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_aux3'] = {
+			label: 'Switch Aux 3',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchaux3',
+					default: '0',
+					choices: this.model.aux3
+				}
+			]
+		};
+	}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_aux4'] = {
+			label: 'Switch Aux 4',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchaux4',
+					default: '0',
+					choices: this.model.aux4
+				}
+			]
+		};
+	}
 
-if (this.config.modelID == 'se700' || this.config.modelID == 'se650') {
-	actions['switch_pip'] = {
-		label: 'Switch PIP Aux',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'switchpip',
-				default: '0',
-				choices: this.model.pip
-			}
-		]
-	};
-}
+	if (this.config.modelID == 'se700' || this.config.modelID == 'se650') {
+		actions['switch_pip'] = {
+			label: 'Switch PIP Aux',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchpip',
+					default: '0',
+					choices: this.model.pip
+				}
+			]
+		};
+	}
 	actions['switch_dsk1'] = {
 		label: 'Switch DSK 1 Aux',
 		options: [
@@ -168,72 +168,72 @@ if (this.config.modelID == 'se700' || this.config.modelID == 'se650') {
 		]
 	};
 
-if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
-	actions['switch_dsk2'] = {
-		label: 'Switch DSK 2 Aux',
+	if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
+		actions['switch_dsk2'] = {
+			label: 'Switch DSK 2 Aux',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'switchdsk2',
+					default: '0',
+					choices: this.model.dsk2
+				}
+			]
+		};
+	}
+	actions['switch_hdmi1'] = {
+		label: 'Switch HDMI 1 Output',
 		options: [
 			{
 				type: 'dropdown',
 				label: 'Input',
-				id: 'switchdsk2',
-				default: '0',
-				choices: this.model.dsk2
+				id: 'switchhdmi1',
+				choices: this.model.hdmi1
 			}
 		]
 	};
-}
-		actions['switch_hdmi1'] = {
-			label: 'Switch HDMI 1 Output',
+
+
+	if (this.config.modelID == 'se3200') {
+		actions['switch_hdmi2'] = {
+			label: 'Switch HDMI 2 Output',
 			options: [
 				{
 					type: 'dropdown',
 					label: 'Input',
-					id: 'switchhdmi1',
-					choices: this.model.hdmi1
+					id: 'switchhdmi2',
+					choices: this.model.hdmi2
 				}
 			]
 		};
-	
-
-		if (this.config.modelID == 'se3200') {
-			actions['switch_hdmi2'] = {
-				label: 'Switch HDMI 2 Output',
-				options: [
-					{
-						type: 'dropdown',
-						label: 'Input',
-						id: 'switchhdmi2',
-						choices: this.model.hdmi2
-					}
-				]
-			};
-		}
-		if (this.config.modelID == 'se3200') {
-			actions['switch_hdmi3'] = {
-				label: 'Switch HDMI 3 Output',
-				options: [
-					{
-						type: 'dropdown',
-						label: 'Input',
-						id: 'switchhdmi3',
-						choices: this.model.hdmi3
-					}
-				]
-			};
-		}
-
-		actions['switch_sdi1'] = {
-			label: 'Switch SDI 1 Output',
+	}
+	if (this.config.modelID == 'se3200') {
+		actions['switch_hdmi3'] = {
+			label: 'Switch HDMI 3 Output',
 			options: [
 				{
 					type: 'dropdown',
 					label: 'Input',
-					id: 'switchsdi1',
-					choices: this.model.sdi1
+					id: 'switchhdmi3',
+					choices: this.model.hdmi3
 				}
 			]
 		};
-	if(this.config.modelID != 'se650'){
+	}
+
+	actions['switch_sdi1'] = {
+		label: 'Switch SDI 1 Output',
+		options: [
+			{
+				type: 'dropdown',
+				label: 'Input',
+				id: 'switchsdi1',
+				choices: this.model.sdi1
+			}
+		]
+	};
+	if (this.config.modelID != 'se650') {
 		actions['switch_sdi2'] = {
 			label: 'Switch SDI 2 Output',
 			options: [
@@ -246,7 +246,7 @@ if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
 			]
 		};
 	}
-	
+
 	actions['trans'] = {
 		label: 'Transition Controls',
 		options: [
@@ -284,46 +284,56 @@ if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
 		]
 	};
 
-	actions['user'] = {
+	actions['loaduser'] = {
 		label: 'Load User',
 		options: [
 			{
-				type: 'dropdown',
-				label: 'Load',
-				id: 'user',
-				default: '0',
-				choices: this.model.user
+				type: 'textinput',
+				label: 'User 1-999',
+				id: 'userid',
+				default: '1'
+			}
+		]
+	};
+	actions['saveuser'] = {
+		label: 'Save User',
+		options: [
+			{
+				type: 'textinput',
+				label: 'User 1-999',
+				id: 'userid',
+				default: '1'
 			}
 		]
 	};
 	if (this.config.modelID != 'se700' && this.config.modelID != 'se650') {
-	actions['streamer'] = {
-		label: 'Streamer Options',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Action',
-				id: 'streamer',
-				default: '0',
-				choices: this.model.streamer
-			}
-		]
-	};
-}
+		actions['streamer'] = {
+			label: 'Streamer Options',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Action',
+					id: 'streamer',
+					default: '0',
+					choices: this.model.streamer
+				}
+			]
+		};
+	}
 
-if (this.config.modelID == 'se3200') {
-	actions['logo'] = {
-		label: 'Logo Controls',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Input',
-				id: 'logo',
-				default: '0',
-				choices: this.model.logo
-			}
-		]
-	};
-}
+	if (this.config.modelID == 'se3200') {
+		actions['logo'] = {
+			label: 'Logo Controls',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Input',
+					id: 'logo',
+					default: '0',
+					choices: this.model.logo
+				}
+			]
+		};
+	}
 	return actions
 }
